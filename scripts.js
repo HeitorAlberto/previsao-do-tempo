40593852c28cf3130c58143f7f44297380206a35
HeitorAlberto <56478574+HeitorAlberto@users.noreply.github.com>
2025-11-13 12:19:18 -0300

    Atualizações

--- scripts.js
+++ scripts.js
@@ -25,7 +25,7 @@ const getAddressText = address => {
     const city = address.city || address.town || address.village || address.municipality || '';
     const state = address.state || '';
     const country = address.country || '';
-    return `${city}${state ? ', ' + state : ''}${country ? ' - ' + country : ''}`;
+    return `${city}${state ? ', ' + state : ''}${country ? ', ' + country : ''}`;
 };
 
 const prepareHourlyArrays = hourly => ({
